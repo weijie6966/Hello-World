cf31901bf6fa5b54b5332f91537aefb885b51ff1
weijie6966 <81344331+weijie6966@users.noreply.github.com>
2021-03-28 21:44:00 +0800

    Add files via upload

---
+++ Python/String/trapezium.py
@@ -0,0 +1,35 @@
+a=10
+b=20
+h=5
+area=0
+
+# formulate of area trapezium
+#tiong wei jie
+#20DDT19F1077
+
+#a is long base
+#b is short base
+#h is height 
+area=(1/2)*(a+b)*h
+print(area)
+
+
+#convert to int format
+c=37.11
+k=0
+
+k=int(c+273.15)
+print(k)
+
+#convert from celcius to farenheit
+f=(9/5)*c+(32)
+print(f)
+
+
+#find the valume of cone
+radius=2
+height=7
+pi=22/7
+
+valumeofcone=(1/3)*pi*radius*radius*height
+print(valumeofcone)
